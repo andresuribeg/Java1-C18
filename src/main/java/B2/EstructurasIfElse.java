411f36862e3d10577bf6d242509b41a5c9ac1109
Andres Uribe <andres.uribe.gutierrez@gmail.com>
2023-09-26 09:17:33 -0500

    Primer commit en develop

---
+++ src/main/java/B2/EstructurasIfElse.java
@@ -0,0 +1,12 @@
+package B2;
+
+public class EstructurasIfElse {
+    public static void main(String[]args){
+
+        //Si entonces SiNo
+//        int edad=18;
+//
+//                if(x)
+
+    }
+}
